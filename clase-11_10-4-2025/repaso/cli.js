62348486df20fec6367a5cbf6fd5022d8a7ba472
gabrielalberini <gabialberini733@gmail.com>
2025-04-10 11:55:54 -0300

    actualizacion clase 11

--- clase-11_10-4-2025/repaso/cli.js
+++ clase-11_10-4-2025/repaso/cli.js
@@ -3,6 +3,7 @@
 // const argumentos = process.argv
 // const argsSinRutasDeNode = argumentos.slice(2)
 // console.log(argsSinRutasDeNode)
+import crypto from "node:crypto"
 
 // id -> primary key -> unico | no se repite | no cambia
 const usuarios = [
@@ -29,7 +30,7 @@ if (action === "help") {
     deleteUser -> borrar usuario
     `)
 } else if (action === "getUsers") {
-
+  console.log(usuarios)
 } else if (action === "getUser") {
   // tomar el id refente al usuario que busco}
   const id = args[1]
@@ -41,11 +42,44 @@ if (action === "help") {
     console.log(usuarioEncontrado, "<- usuario encontrado")
   }
 } else if (action === "createUser") {
+  const id = crypto.randomUUID()
+  const name = args[1]
 
+  const nuevoUsuario = { id, name }
+  usuarios.push(nuevoUsuario)
+  console.log(usuarios)
 } else if (action === "updateUser") {
+  const id = args[1]
+  const nuevoNombre = args[2]
 
+  const usuarioEncontrado = usuarios.find(u => u.id === id)
+
+  if (!usuarioEncontrado) {
+    console.log("Usuario no encontrado")
+  }
+
+  usuarioEncontrado.name = nuevoNombre
+  console.log(usuarioEncontrado)
 } else if (action === "deleteUser") {
+  // solución 1 (costosa)
+  const id = args[1]
+  const usuarioEncontrado = usuarios.find((usuario) => usuario.id === id)
+  if (usuarioEncontrado === undefined) {
+    console.log("Usuario no encontrado...")
+  } else {
+    const index = usuarios.indexOf(usuarioEncontrado)
+    usuarios.splice(index, 1)
+    console.log(usuarioEncontrado, "<- usuario borrado")
+  }
 
+  // solución 2 (menos costosa)
+  const index = usuarios.findIndex((u) => u.id === id)
+  if (index === -1) {
+    console.log("Usuario no encontrado...")
+  } else {
+    const usuarioBorrado = usuarios.splice(index, 1)
+    console.log(usuarioBorrado, "<- usuario borrado")
+  }
 } else {
   console.log("Necesitas permisos...")
 }
