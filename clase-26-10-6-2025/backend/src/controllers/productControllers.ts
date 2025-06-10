7d33cf5144c6161702d8d3f045a4ffe1f20ff676
gabrielalberini <gabialberini733@gmail.com>
2025-06-10 10:59:30 -0300

    clase 26 - incorporación de validadores en front y back

---
+++ clase-26-10-6-2025/backend/src/controllers/productControllers.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express"
+import { Product } from "../models/productModel"
+
+const getAllProducts = async (req: Request, res: Response): Promise<any> => {
+  const user = req.user
+  console.log(user, "<- usuario logueado desde el controlador")
+
+  try {
+    const products = await Product.find()
+    res.json({
+      success: true,
+      message: "recuperar todos los productos",
+      data: products
+    })
+  } catch (error) {
+    const err = error as Error
+    res.status(500).json({ success: false, message: err.message })
+  }
+}
+
+export { getAllProducts }
---
+++ clase-26-10-6-2025/frontend/src/views/Home/Home.jsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react"
+import { Layout } from "../../components/Layout"
+import { Link } from "react-router-dom"
+import { useAuth } from "../../context/AuthContext"
+
+const Home = () => {
+  const [products, setProducts] = useState([])
+  const [error, setError] = useState(null)
+
+  const { user, logout } = useAuth()
+
+  useEffect(() => {
+    const fetchingProducts = async () => {
+      const token = localStorage.getItem("token")
+      try {
+        const response = await fetch("http://localhost:1234/api/products", {
+          headers: { Authorization: `Bearer ${token}` }
+        }
+        )
+
+        if (!response.ok) {
+          setError("Sesión terminada, vuelve a loguearte.")
+          logout()
+          // continuar controlando el home como ruta privada
+          throw new Error("Falló el fetch :(")
+        }
+        const dataProducts = await response.json()
+
+        setProducts(dataProducts.data)
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+
+    fetchingProducts()
+  }, [])
+
+  return (
+    <Layout>
+      <h1>Lista de productos</h1>
+      {user && <p>Bienvenido, {user.email}</p>}
+      {error && <>
+        <h2>{error}</h2>
+        <Link to={"/login"}>Ir al login</Link>
+      </>}
+      {
+        products.map((product) => {
+          return (
+            <div key={product._id}>
+              <h2>{product.name}</h2>
+              <p>{product.price}</p>
+              <p>{product.category}</p>
+            </div>
+          )
+        })
+      }
+    </Layout>
+  )
+}
+
+export { Home }
